7339172c3023b037c134d96820282507f2d94f71
Irwanzz <irwanshahiman4@gmail.com>
2026-07-22 22:30:58 +0800

    Add files via upload

---
+++ CheckOddEvenNumbers/OddEven.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OddEven
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int number;
+
+        System.out.print("Enter number : ");
+        number = input.nextInt();
+
+        if(number % 2 == 0)
+        {
+            System.out.println("Even Number");
+        }
+        else
+        {
+            System.out.println("Odd Number");
+        }
+    }
+}
